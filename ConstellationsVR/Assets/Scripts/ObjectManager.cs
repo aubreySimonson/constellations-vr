2b1f2af04a095f812205fa1113b9c1fc83fe3d64
Mateo Larrea <mlarreaf99@gmail.com>
2022-12-06 21:42:54 -0500

    ObjectManager + ObjectLogic

--- ConstellationsVR/Assets/Scripts/ObjectManager.cs
+++ ConstellationsVR/Assets/Scripts/ObjectManager.cs
@@ -8,29 +8,28 @@ public class ObjectManager : MonoBehaviour
     //[SerializeField] GameObject CsoundEditor; // Object where sample is attached
     [SerializeField] Transform startingPosition;
     [SerializeField] GameObject CsoundObject;
-    int numberOfCsoundUnityInstances = 0;
+    public int numberOfCsoundUnityInstances = 0;
     int numberOfStudent;
-    
+
+    // Tracking Current Objects in Scene
+    [SerializeField] GameObject[] inScene;
 
     // Sample Mgmt
     public bool vocalMode = true; // vocal Samples are displayed (true), project samples are displayed (false)
-    [SerializeField] GameObject sampleObjectStudent;
-    [SerializeField] GameObject sampleObjectTeachingTeam;
+    [SerializeField] GameObject sampleObject;
 
 
     // Audio Source for interface (plays sample when hovering over name)
-    [SerializeField] AudioSource _source; 
+    [SerializeField] AudioSource _source;
 
 
     // Students - Data Structure
     [SerializeField] Student[] students;
 
 
-
-    private void Start()
-    {
-        numberOfStudent = students.Length;
-    }
+    // Positioning
+    [SerializeField] Vector3[] SchoolPosition; // 0 = Berklee, 1 = Harvard, 2 = MIT
+    [SerializeField] Vector3[] GroupPosition; // 0 = Aura, 1 = Magic, 2 = MusiCraft, 3 = Sonic Soup, StageFreight
 
 
     public void InstantiateCsoundObject()
@@ -57,7 +56,7 @@ public class ObjectManager : MonoBehaviour
         }
         else
         {
-            int correctID = (int) numberOfStudent / 2 + id; //Allows to have one single Csound.Load
+            int correctID = (int)numberOfStudent / 2 + id; //Allows to have one single Csound.Load
             _source.clip = students[correctID].projectSample;
         }
 
@@ -71,25 +70,79 @@ public class ObjectManager : MonoBehaviour
 
     public void SelectingSample(int id)
     {
-        if (students[id].teachingTeam)
-        {
-            GameObject instance = Instantiate(sampleObjectTeachingTeam);
-            UpdateInformation(id, instance);
-        }
-        else
-        {
-            GameObject instance = Instantiate(sampleObjectStudent);
-            UpdateInformation(id, instance);
-        }
+
+        GameObject instance = Instantiate(sampleObject);
+        UpdateInformation(id, instance);
 
     }
 
     void UpdateInformation(int id, GameObject instance)
     {
         instance.name = students[id].name;
-        instance.tag = students[id].school; // Remember to add these tags to the project
+        instance.tag = "Sound Object"; // Don't forget to add this tag to project
         instance.GetComponent<SampleSelector>()._id = id;
+        instance.GetComponent<ObjectLogic>().studentData = students[id]; // Don't forget to add ObjectLogic.cs to sample object
 
     }
 
+    public void Sorting(int mode) // s = school, g = group, r = random
+    {
+        UpdateCurrentObjectsInScene();
+
+
+        switch (mode)
+        {
+            case 0: // Scool Sorting
+                for (int i = 0; i < inScene.Length; i++)
+                {
+                    string school = inScene[i].GetComponent<ObjectLogic>().studentData.school;
+
+                    switch (school)
+                    {
+                        case "Berklee":
+                            Vector3 offset = new Vector3(Random.Range(0, 3), Random.Range(0, 3), Random.Range(0, 3));
+                            Vector3 result = SchoolPosition[0] + offset;
+                            StartCoroutine(LerpPosition(result, 5, inScene[i]));
+
+                            break;
+                        case "Harvard":
+                            break;
+
+                        case "MIT":
+                            break;
+
+                        case "Null":
+                            break;
+                    }
+                }
+
+                break;
+            case 1: // Group Sorting
+
+                break;
+            case 2: // Random
+
+                break;
+        }
+    }
+
+    void UpdateCurrentObjectsInScene()
+    {
+        inScene = GameObject.FindGameObjectsWithTag("Sound Object");
+    }
+
+    IEnumerator LerpPosition(Vector3 targetPosition, float duration, GameObject currentObject)
+    {
+        float time = 0;
+        Vector3 startPosition = currentObject.transform.position;
+        while (time < duration)
+        {
+            currentObject.transform.position = Vector3.Lerp(startPosition, targetPosition, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        currentObject.transform.position = targetPosition;
+    }
+
 }
+
